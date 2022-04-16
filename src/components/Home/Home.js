61a0d444f6a05d320f6a981945aa6ccff3a506f6
khadiza21 <bibikhadiza474@gmail.com>
2022-04-16 11:23:36 +0600

    react hook firebase router

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import useFirebase from '../../hooks/useFirebae';
+import { getAuth } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
 
 const Home = () => {
-    const {user} = useFirebase();
-    return (
-        <div>
-            <h2>THis is home</h2>
-            <p>Current User is: {user ? user.displayName:"no User"}</p>
-        </div>
-    );
+  const [user] = useAuthState(auth);
+  return (
+    <div>
+      <h2>THis is home</h2>
+      <p>Current User is: {user ? user.displayName : "no User"}</p>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,17 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
-import useFirebase from "../../hooks/useFirebae";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import app from "../../firebase.init";
 
+const auth = getAuth(app);
 const Login = () => {
-    const {signInWithGoogle} = useFirebase();
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
   return (
     <div>
       <h3>Please LogIn</h3>
 
       <div style={{ margin: "20px" }}>
-        <button onClick={signInWithGoogle}>Google Sign In</button>
+        <button onClick={() => signInWithGoogle()}>Google Sign In</button>
       </div>
 
       <form>
