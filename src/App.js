411fd2d352eb33c6354d23cb29f9e0bf6cd79c56
khadiza21 <bibikhadiza474@gmail.com>
2022-04-16 13:03:10 +0600

    react hook firebase router

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ import Home from "./components/Home/Home";
 import Products from "./components/Products/Products";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Orders from "./components/Orders/Orders";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 function App() {
   return (
     <div className="App">
-     <Header></Header>
+      <Header></Header>
       <Routes>
-        <Route path="/" element={<Home></Home>}>
-          Home
-        </Route>
-        <Route path="/products" element={<Products></Products>}>
-          Home
-        </Route>
-        <Route path="/login" element={<Login></Login>}>
-          LogIn
-        </Route>
-        <Route path="/register" element={<Register></Register>}>
-          LogIn
-        </Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route
+          path="/products"
+          element={
+            <RequireAuth>
+              <Products></Products>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders></Orders>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,24 +5,27 @@ import { Link } from "react-router-dom";
 import app from "../../firebase.init";
 import "./Header.css";
 
-
 const auth = getAuth(app);
 
-
-
 const Header = () => {
-  const [user ] = useAuthState(auth);
- 
+  const [user] = useAuthState(auth);
+
   return (
     <div className={"header"}>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
         <Link to="/orders">Orders</Link>
+        {user && (
+          <>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </>
+        )}
         <Link to="/register">Register</Link>
         <span>{user?.displayName && user.displayName}</span>
         {user?.uid ? (
-          <button onClick={() =>  signOut(auth)}>Sign Out</button>
+          <button onClick={() => signOut(auth)}>Sign Out</button>
         ) : (
           <Link to="/login">Login</Link>
         )}
